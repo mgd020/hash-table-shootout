9da1f8ff9dfb313b8b8e6256257c4d4ade014063
Matthew Downey <mgd020@gmail.com>
2016-10-02 23:03:55 +1100

    improvements

--- src/custom.cc
+++ src/custom.cc
@@ -5,9 +5,11 @@
 #include <functional> // hash
 #include <cstdlib> // malloc, realloc, free
 #include <stdexcept> // out_of_range
-#include <cstrings> // bzero
+#include <strings.h> // bzero
+#include <cassert> // assert
 
 
+#include <ctime>
 #include <iostream>
 
 
@@ -15,13 +17,13 @@ template <class K, class V, class H = std::hash<K>, class P = std::equal_to<K> >
 class Custom {
 public:
 
-    explicit Custom(size_t capacity = 1):
-        _h(capacity),
-        _kv(capacity),
+    typedef std::pair<K, V> value_type;
+
+    explicit Custom(size_t capacity=4):
         _capacity(capacity),
         _size(0) {
-
-        bzero(&_h[0], sizeof(size_t) * capacity);
+        assert(capacity);
+        alloc();
     }
 
     ~Custom() {
@@ -30,10 +32,16 @@ public:
                 destruct(_kv[i]);
             }
         }
+        free(_h);
+        free(_kv);
     }
 
     size_t size() const {
         return _size;
+    }
+
+    size_t capacity() const {
+        return _capacity;
     }
 
     bool empty() const {
@@ -57,15 +65,15 @@ public:
             }
 
             if (hash_i == h) {
-                std::pair<K, V> & kv = _kv[i];
+                value_type & kv = _kv[i];
                 if (keys_equal(k, kv.first)) {
                     return kv.second;
                 }
-            }
-
-            size_t dist_i = probe_distance(hash_i, i);
-            if (dist > dist_i) {
-                throw std::out_of_range("");
+            } else {
+                size_t dist_i = probe_distance(hash_i, i);
+                if (dist > dist_i) {
+                    throw std::out_of_range("");
+                }
             }
 
             i = (i + 1) % _capacity;
@@ -77,90 +85,64 @@ public:
         return const_cast<Custom *>(this)->get(k);
     }
 
-    void set(std::pair<K, V> && kv) {
-        size_t h = hash_key(kv.first);
-        size_t i = bucket(h);
-        size_t dist = 0;
-        bool rehashed = false;
-
-        while (true) {
-            size_t hash_i = _h[i];
-
-            if (!hash_i) {
-                if (!rehashed) {
-                    ++_size;
-                    if (rehash()) {
-                        i = bucket(h);
-                        continue;
-                    }
-                }
-                _h[i] = h;
-                new (&_kv[i]) std::pair<K, V>(kv);
-                return;
-            }
-
-            if (hash_i == h) {
-                std::pair<K, V> & kv_i = _kv[i];
-                if (keys_equal(kv.first, kv_i.first)) {
-                    kv_i.second = std::move(kv.second);
-                    return;
-                }
-            }
-
-            size_t dist_i = probe_distance(hash_i, i);
-            if (dist > dist_i) {
-                if (!rehashed) {
-                    ++_size;
-                    if (rehash()) {
-                        i = bucket(h);
-                        continue;
-                    }
-                }
-                std::swap(_h[i], h);
-                std::swap(_kv[i], kv);
-                dist = dist_i;
-            }
-
-            i = (i + 1) % _capacity;
-            ++dist;
-        }
+    void set(value_type && kv) {
+        _set(hash_key(kv.first), std::move(kv));
     }
 
     void del(const K & k) {
         if (!_size) {
-            throw std::out_of_range("");
+            return;
         }
 
         size_t h = hash_key(k);
         size_t i = bucket(h);
         size_t dist = 0;
-        bool rehashed = false;
 
         while (true) {
             size_t hash_i = _h[i];
 
             if (!hash_i) {
-                throw std::out_of_range("");
+                return;
             }
 
             if (hash_i == h) {
-                std::pair<K, V> & kv = _kv[i];
+                value_type & kv = _kv[i];
                 if (keys_equal(k, kv.first)) {
-                    _h[i] = 0;
                     destruct(kv);
+                    _h[i] = 0;
                     --_size;
-                    rehash();
+                    break;
+                }
+            } else {
+                size_t dist_i = probe_distance(hash_i, i);
+                if (dist > dist_i) {
                     return;
                 }
             }
 
-            size_t dist_i = probe_distance(hash_i, i);
-            if (dist > dist_i) {
-                throw std::out_of_range("");
-            }
-
             i = (i + 1) % _capacity;
             ++dist;
+        }
+
+        if (_size < _shrink) {
+            rehash(_capacity / 2);
+        } else {
+            while (true) {
+                i = (i + 1) % _capacity;
+
+                size_t hash_i = _h[i];
+                if (!hash_i) {
+                    break;
+                }
+
+                size_t dist_i = probe_distance(hash_i, i);
+                if (!dist_i) {
+                    break;
+                }
+
+                std::swap(_h[i], _h[i - 1]);
+                std::swap(_kv[i], _kv[i - 1]);
+            }
         }
     }
 
@@ -168,40 +150,77 @@ public:
         return 1.0 * _size / _capacity;
     }
 
-    bool rehash() {
-        size_t capacity;
+// private:
 
-        if (_size == _capacity) {
-            capacity = _capacity * 2;
-        // } else if (_size < _capacity / 4) { // TODO: fix this, its broken
-        //     capacity = _capacity / 2;
-        } else {
-            return false;
-        }
+    void rehash(size_t new_capacity) {
+        auto old_capacity = _capacity;
+        auto h = _h;
+        auto kv = _kv;
 
-        Custom c(capacity);
+        _capacity = new_capacity;
+        _size = 0;
+        alloc();
 
-        for (size_t i = 0; i < _capacity; ++i) {
-            if (_h[i]) {
-                c.set(std::move(_kv[i]));
-                _h[i] = 0;
+        for (size_t i = 0; i < old_capacity; ++i) {
+            if (h[i]) {
+                _set(h[i], std::move(kv[i]));
             }
         }
 
-        std::swap(_h, c._h);
-        std::swap(_kv, c._kv);
-        c._capacity = _capacity;
-        _capacity = capacity;
-        _size = c._size;
-        c._size = 0;
-        return true;
+        free(h);
+        free(kv);
     }
 
-// private:
+    void alloc() {
+        _h = (size_t *)malloc(sizeof(size_t) * _capacity);
+        _kv = (value_type *)malloc(sizeof(value_type) * _capacity);
+        bzero(_h, sizeof(size_t) * _capacity);
+        _grow = 75 * _capacity / 100;
+        _shrink = 75 * _capacity / 400;
+    }
+
+    void _set(size_t h, value_type && kv) {
+        size_t i = bucket(h);
+        size_t dist = 0;
+
+        if (_size >= _grow) {
+            rehash(_capacity * 2);
+        }
+
+        while (true) {
+            size_t hash_i = _h[i];
+
+            if (!hash_i) {
+                new (&_kv[i]) value_type(kv);
+                _h[i] = h;
+                ++_size;
+                return;
+            }
+
+            if (hash_i == h) {
+                value_type & kv_i = _kv[i];
+                if (keys_equal(kv.first, kv_i.first)) {
+                    kv_i.second = std::move(kv.second);
+                    return;
+                }
+            } else {
+                size_t dist_i = probe_distance(hash_i, i);
+                if (dist > dist_i) {
+                    std::swap(_h[i], h);
+                    std::swap(_kv[i], kv);
+                    dist = dist_i;
+                }
+            }
+
+            i = (i + 1) % _capacity;
+            ++dist;
+        }
+    }
+
     inline static size_t hash_key(const K & k) {
         static H h;
         size_t hk = h(k);
-        return hk | (hk == 0); // 0 reserved for empty
+        return hk ?: -1; // 0 reserved for empty
     }
 
     inline size_t bucket(size_t h) {
@@ -222,27 +241,12 @@ public:
         t.~T();
     }
 
-    template <class T>
-    class Array {
-        T * __restrict _buffer;
-    public:
-        inline Array(): _buffer(NULL) {}
-        inline explicit Array(size_t s): _buffer(s ? (T *)malloc(sizeof(T) * s) : NULL) {}
-        inline Array(const Array &) = delete;
-        inline Array & operator=(const Array &) = delete;
-        inline Array(Array && a): _buffer(a._buffer) { a._buffer = NULL; }
-        inline Array & operator=(Array && a) { std::swap(_buffer, a._buffer); return *this; }
-        inline T & operator[](size_t i) { return _buffer[i]; }
-        inline const T & operator[](size_t i) const { return _buffer[i]; }
-        inline void resize(size_t s) { _buffer = (T *)realloc(_buffer, sizeof(T) * s); }
-        inline ~Array() { free(_buffer); }
-    };
-
-    Array<size_t> _h; // hashes
-    Array<std::pair<K, V> > _kv; // key value pairs
-
+    size_t *_h;
+    value_type *_kv;
     size_t _capacity; // length of arrays
     size_t _size; // number of items stored
+    size_t _grow;
+    size_t _shrink;
 };
 
 
@@ -251,30 +255,32 @@ typedef Custom<int64_t, int64_t> hash_t;
 typedef Custom<const char *, int64_t> str_hash_t;
 #define SETUP hash_t hash; str_hash_t str_hash;
 #define INSERT_INT_INTO_HASH(key, value) hash.set(std::make_pair(key, value))
-#define DELETE_INT_FROM_HASH(key) try { hash.del(key); } catch (std::out_of_range &) { std::cout << "missing key " << key << std::endl; }
+#define DELETE_INT_FROM_HASH(key) try { hash.del(key); } catch (std::out_of_range &) {}
 #define INSERT_STR_INTO_HASH(key, value) str_hash.set(std::make_pair(key, value))
-#define DELETE_STR_FROM_HASH(key) try { str_hash.del(key); } catch (std::out_of_range &) { std::cout << "missing key " << key << std::endl; }
+#define DELETE_STR_FROM_HASH(key) try { str_hash.del(key); } catch (std::out_of_range &) {}
 
 #if 1
 #include "template.c"
 #else
 
 int main() {
+    srand(time(0));
     using namespace std;
     SETUP
     cout << "in:" << endl;
     for(int i = 0; i < 20; i++) {
-        int k = random() % 20;
+        int k = random() % 200;
         cout << k << " " << flush;
         INSERT_INT_INTO_HASH(k, 0);
-        for (size_t i = 0; i < hash._capacity; ++i) {
-            if (hash._h[i]) {
-                cout << hash._h[i] << ' ';
-            } else {
-                cout << '.' << ' ';
-            }
-        }
-        cout << endl;
+        INSERT_INT_INTO_HASH(k + 1, 0);
+        // for (size_t i = 0; i < hash._capacity; ++i) {
+        //     if (hash._h[i]) {
+        //         cout << hash._h[i] << ' ';
+        //     } else {
+        //         cout << '.' << ' ';
+        //     }
+        // }
+        // cout << endl;
     }
     cout << endl;
     cout << "\nout:" << endl;
@@ -287,6 +293,23 @@ int main() {
     }
     cout << endl;
     // 0 1 2 3 6 7 9 10 12 13 15 16 17 19
+    while (hash.size() > 1) {
+        int k = random() % 200;
+        size_t old_size = hash.size();
+        DELETE_INT_FROM_HASH(k);
+        if (hash.size() == old_size) {
+            continue;
+        }
+        cout << "delete:" << k << endl;
+        for (size_t i = 0; i < hash._capacity; ++i) {
+            if (hash._h[i]) {
+                cout << hash._h[i] << '(' << hash.probe_distance(hash._h[i], i) << ") ";
+            } else {
+                cout << '.' << ' ';
+            }
+        }
+        cout << endl;
+    }
 }
 
 #endif
